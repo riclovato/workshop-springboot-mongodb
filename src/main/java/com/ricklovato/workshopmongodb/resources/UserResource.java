f2c585402a5ae5021fe8678cf487c968c21f74f3
riclovato <riic.lovato@gmail.com>
2023-03-01 15:36:59 -0300

    obtendo um usuario por id

--- src/main/java/com/ricklovato/workshopmongodb/resources/UserResource.java
+++ src/main/java/com/ricklovato/workshopmongodb/resources/UserResource.java
@@ -1,7 +1,6 @@
 package com.ricklovato.workshopmongodb.resources;
 
 import java.util.List;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 import org.slf4j.Logger;
@@ -9,7 +8,9 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ricklovato.workshopmongodb.domain.User;
@@ -32,5 +33,11 @@ public class UserResource {
 		List<User> list = service.findAll();
 		List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(listDto);
+	}
+	
+	@RequestMapping(value="/{id}", method = RequestMethod.GET)
+	public ResponseEntity<UserDTO> findById(@PathVariable String id) {
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(new UserDTO(obj));
 	}
 }
--- src/main/java/com/ricklovato/workshopmongodb/services/UserService.java
+++ src/main/java/com/ricklovato/workshopmongodb/services/UserService.java
@@ -1,20 +1,38 @@
 package com.ricklovato.workshopmongodb.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.ricklovato.workshopmongodb.domain.User;
 import com.ricklovato.workshopmongodb.repository.UserRepository;
+import com.ricklovato.workshopmongodb.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
 
+	// Injeção de dependência da interface UserRepository
 	@Autowired
 	private UserRepository repo;
-	
+
+	// Retorna todos os usuários cadastrados
 	public List<User> findAll(){
 		return repo.findAll();
 	}
+
+	// Busca um usuário por id
+	public User findById(String id) {
+		// Busca o usuário no banco de dados pelo id
+		Optional<User> obj = repo.findById(id);
+		// Retorna o usuário encontrado ou lança uma exceção ObjectNotFoundException se o usuário não foi encontrado
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	/*
+	 * o Optional<User> é usado para representar o resultado da busca de um usuário pelo seu id 
+	 * na base de dados. Se o usuário é encontrado, o Optional irá conter o objeto User correspondente; caso contrário, o Optional será vazio. 
+	 * O método orElseThrow é chamado para obter o valor contido no Optional, ou lançar uma exceção do tipo ObjectNotFoundException se o Optional estiver vazio, 
+	 * indicando que o objeto não foi encontrado na base de dados.
+	 */
 }
