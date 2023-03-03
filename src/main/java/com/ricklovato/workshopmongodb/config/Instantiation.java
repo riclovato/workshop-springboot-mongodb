c4dfd2b923abcdc904fdbf20ef7520ccf3446bf7
riclovato <riic.lovato@gmail.com>
2023-03-03 19:26:54 -0300

    Projecao dos dados do autor com DTO

--- src/main/java/com/ricklovato/workshopmongodb/config/Instantiation.java
+++ src/main/java/com/ricklovato/workshopmongodb/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.ricklovato.workshopmongodb.domain.Post;
 import com.ricklovato.workshopmongodb.domain.User;
+import com.ricklovato.workshopmongodb.dto.AuthorDTO;
 import com.ricklovato.workshopmongodb.repository.PostRepository;
 import com.ricklovato.workshopmongodb.repository.UserRepository;
 @Configuration
@@ -35,13 +36,13 @@ public class Instantiation implements CommandLineRunner {
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
-		
-	
-	
-		Post post1 = new Post(null,sdf.parse("21/03/2018"),"Partiu Viagem!","Vou viajar para São Paulo. Abraços",maria);
-	
-		Post post2 = new Post(null,sdf.parse("23/03/2018"),"Bom dia","Acordei feliz hoje!", maria);
 		userRepository.saveAll(Arrays.asList(maria,alex,bob));
+	//salvar primeiro os usuarios para que eles tenham uma ID própria antes de salvar nos posts
+	
+		Post post1 = new Post(null,sdf.parse("21/03/2018"),"Partiu Viagem!","Vou viajar para São Paulo. Abraços",new AuthorDTO(maria));
+	
+		Post post2 = new Post(null,sdf.parse("23/03/2018"),"Bom dia","Acordei feliz hoje!", new AuthorDTO(maria));
+	
 		postRepository.saveAll(Arrays.asList(post1,post2));
 	}
 
