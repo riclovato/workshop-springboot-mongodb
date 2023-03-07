a83aec1d06db72ab44bd277b37e15e4d4afa9ca6
riclovato <riic.lovato@gmail.com>
2023-03-07 16:23:05 -0300

    Consulta simples com @Querry

--- src/main/java/com/ricklovato/workshopmongodb/repository/PostRepository.java
+++ src/main/java/com/ricklovato/workshopmongodb/repository/PostRepository.java
@@ -3,6 +3,7 @@ package com.ricklovato.workshopmongodb.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.ricklovato.workshopmongodb.domain.Post;
@@ -12,4 +13,7 @@ public interface PostRepository extends MongoRepository<Post, String> {
 
 	//só esse declaração já faz com que o spring data monte a consulta
 	List<Post> findByTitleContainingIgnoreCase(String text);
+	
+	@Query("{ 'title': { $regex: ?0, $options: 'i'} }")
+	List<Post> searchTitle(String text);
 }
