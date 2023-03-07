76081363148900cdd7dc466ddd978591dad2fbf7
riclovato <riic.lovato@gmail.com>
2023-03-07 15:08:53 -0300

    Acrescentando comentarios aos posts

--- src/main/java/com/ricklovato/workshopmongodb/domain/Post.java
+++ src/main/java/com/ricklovato/workshopmongodb/domain/Post.java
@@ -1,13 +1,16 @@
 package com.ricklovato.workshopmongodb.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.ricklovato.workshopmongodb.dto.AuthorDTO;
+import com.ricklovato.workshopmongodb.dto.CommentDTO;
 @Document
 public class Post implements Serializable{
 
@@ -18,6 +21,8 @@ public class Post implements Serializable{
 	private String title;
 	private String body;
 	private AuthorDTO author;
+	
+	private List<CommentDTO> comments = new ArrayList<>();
 	
 	public Post() {
 		
@@ -72,6 +77,14 @@ public class Post implements Serializable{
 		this.author = author;
 	}
 	
+	public List<CommentDTO> getComments() {
+		return comments;
+	}
+
+	public void setComments(List<CommentDTO> comments) {
+		this.comments = comments;
+	}
+	
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
@@ -88,6 +101,8 @@ public class Post implements Serializable{
 		Post other = (Post) obj;
 		return Objects.equals(id, other.id);
 	}
+
+	
 
 	
 }
